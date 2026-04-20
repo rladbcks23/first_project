39936332a73d57d4266d93ad1405281a7246e036
김유찬 <a01030163474@gmail.com>
2026-04-20 17:12:38 +0900

    add vedio file practice

--- testimg/views.py
+++ testimg/views.py
@@ -2,22 +2,31 @@ from django.shortcuts import render
 from django.core.files.storage import default_storage
 
 def image(request):
-    if request.method == 'POST' and request.FILES.get('image'):
-        file = request.FILES['image']
-        # file = request.FILES['image']
+    if request.method == 'POST':        # 이미지 파일
+        if request.FILES.get('image'):
+            file = request.FILES['image']
+            
+            filename = default_storage.save(file.name, file)
+            file_url = default_storage.url(filename)
+            print(filename)
+            print(file_url)
+            print(type(default_storage))
 
-        filename = default_storage.save(file.name, file)
-        file_url = default_storage.url(filename)
-        print(filename)
-        print(file_url)
-        print(type(default_storage))
-        # saved_path = default_storage.save(f'uploads/{file.name}', file)
-        # file_url = default_storage.url(saved_path)
+            return render(request, 'index.html', {
+                'file_url': file_url,
+            })
+        
+        elif request.FILES.get('video'):    # 영상파일
+            file = request.FILES['video']
 
+            filename = default_storage.save(file.name, file)
+            file_url = default_storage.url(filename)
+            print(filename)
+            print(file_url)
+            print(type(default_storage))
 
-        return render(request, 'index.html', {
-            'file_url': file_url,
-            # 'saved_path': saved_path,
-        })
+            return render(request, 'index.html', {
+                'file_url': file_url,
+            })
 
     return render(request, 'index.html')
