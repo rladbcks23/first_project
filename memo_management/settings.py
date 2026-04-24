f6e64a0e186f62a2c49803f7a31963bad2fe7e77
김유찬 <a01030163474@gmail.com>
2026-04-24 16:32:31 +0900

    edit DEBUG for debug

--- memo_management/settings.py
+++ memo_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)f=!d@3_e8o7j(k%5(bw*(lard)-0maf-hh7^1x_br_h-7@t*#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '*' ]
 
