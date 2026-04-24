caa30ddc72f136104d141ca14b86d2d171851713
김유찬 <a01030163474@gmail.com>
2026-04-24 16:56:20 +0900

    edit setting for db

--- memo_management/settings.py
+++ memo_management/settings.py
@@ -178,6 +178,7 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
     )
 }
